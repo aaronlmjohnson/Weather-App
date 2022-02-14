260318179e71cd80c915be4a97fced389337c92f
Aaron <ajonaaronajon@yahoo.com>
2022-02-14 12:13:24 -0600

    basic dropdown menu completed

--- src/dropDownMenu.js
+++ src/dropDownMenu.js
@@ -1,9 +1,11 @@
-import { div, a} from './dom';
+import { div, a, button} from './dom';
 
 export const dropDownMenu = (name, optionsArr)=>{
     
     const menu = div(name, true);
+    const menuButton = button('menu-button', false, '☰');
     menu.classList.add('dropdown');
+    menu.append(menuButton);
     _createOptions(optionsArr, menu);
 
     return menu;
@@ -12,9 +14,28 @@ export const dropDownMenu = (name, optionsArr)=>{
 const _createOptions = (optionsArr, menu)=>{
     optionsArr.forEach((option)=>{
         const optionElement = div(option, true);
+        optionElement.classList.add('menu-item', 'hidden-menu-item');
+       
         const link = a('dropdown-link', false, option, '#');
         optionElement.append(link);
-
         menu.append(optionElement);
     });
 }
+
+export const toggleMenu = (e)=>{
+    const menu = e.target.parentElement;
+    const menuItems = [...menu.getElementsByClassName('menu-item')];
+
+    menuItems.forEach((menuItem)=>{
+        if(menuItem.classList.contains('hidden-menu-item')){
+            menuItem.classList.remove('hidden-menu-item');
+            menuItem.classList.add('visible-menu-item');
+        } else {
+            menuItem.classList.remove('visible-menu-item');
+            menuItem.classList.add('hidden-menu-item');  
+        }
+    });
+}
+
+export const getDropdownMenuButton = (menuId)=>
+    document.getElementById(menuId).getElementsByClassName('menu-button')[0];
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './assets/styles/style.css';
 
-import { dropDownMenu } from './dropDownMenu';
+import { dropDownMenu, toggleMenu, getDropdownMenuButton} from './dropDownMenu';
 
 const content = document.getElementById('content');
 
 const mainMenu = dropDownMenu('main', ['Home', 'About', 'Buy', 'Sell', 'Contact Us']);
 content.append(mainMenu);
+
+getDropdownMenuButton('main').addEventListener('click', toggleMenu);
 
