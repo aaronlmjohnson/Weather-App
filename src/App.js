4814258e068338186ef447e18b2c0b0b7caf2d9f
Aaron <ajonaaronajon@yahoo.com>
2022-08-07 19:20:19 -0500

    feat: added LocationSearchForm and DropDownMenu components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import useLoadLocationAPI from './components/useLoadLocationAPI';
 import Navbar from './components/Navbar';
 
 function App() {
-
   const { weatherData, loadWeatherData } = useLoadWeatherAPI();
   const { locationData, loadLocationData } = useLoadLocationAPI();
-
-  //1. make components
-  //2. pass hard-coded props
-  //3. create methods to handle passed props
-  //4. add state
 
   return (
     <div className="App">
---
+++ src/components/DropDownMenu.js
@@ -0,0 +1,13 @@
+import "./DropDownMenu.css";
+
+const DropDownMenu = ()=>{
+    return (
+        <ul className="drop-down-menu">
+            <li><a href="#">Thing 1</a></li>
+            <li><a href="#">Thing 2</a></li>
+            <li><a href="#">Thing 3</a></li>
+        </ul>
+    );
+}
+
+export default DropDownMenu;
